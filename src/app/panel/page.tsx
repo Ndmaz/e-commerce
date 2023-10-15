aadd52c9728a73c05c2a3ad115cfc4a7388df4e4
Navid <navid.b.maz@gmail.com>
2023-10-15 10:11:13 +0330

    the git ignore bug

---
+++ src/app/panel/page.tsx
@@ -0,0 +1,30 @@
+import { Card, CardTitle } from "@/components/ui/card";
+
+
+
+export default function panel(){
+
+    return <div className="mt-[5rem]">
+        <Card className="">
+<CardTitle>
+    پنل کاربری
+</CardTitle>
+    
+panel
+<div>
+     سبد خرید
+
+</div>
+
+
+<div>تغییر اطلاعات پروفایل، </div>
+
+admin access
+
+<div>،سفارشات، 
+ </div>
+<div>مدیریت محصولات  
+</div>    
+</Card>
+ </div>
+}
