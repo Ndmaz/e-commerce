711c7f06b2f00b01f87675ce3a583dfc3ec335e1
Navid <navid.b.maz@gmail.com>
2023-12-09 13:31:03 +0330

    Refactor:product fetch inside useeffect

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,19 +1,53 @@
+'use client'
+
 import Productcard from "@/components/Productcard"
-import { Item } from "@radix-ui/react-select"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
  
 
-export default async function Products(){
+export default  function Products(){
 
-const data= await productfetch()
-const products =await data.products
 
-  
-    return<div >
+const [productss,setproducts]=useState([])
+useEffect( () => {
+    
+    async function productfetch () {
+        try {
+            //getting products
+              const res = await fetch('http://localhost:3000/api/gettingproducts')
+        if(res.ok){
+           
+             const data= await res.json()
+             const producti=data.products
+             console.log(producti)
+               console.log(productss)
+           setproducts(producti)
+        
+        }
+      
+        } catch (error) {
+                 alert(error)
+    return{error:error}
+    
+        }
+    
+      
+     
+       
+    }
+    productfetch()
+
+},[])
+useEffect(() => {
+    console.log('Updated productss:', productss);
+  }, [productss])
+
+
+    return<div className="mb-[6rem]">
+
         products
-        <div className="flex">
-     { products.map((item)=>{
+        <div className="flex flex-wrap">
+     { productss.map((item)=>{
        return( <Productcard
        key={item.id}
        productname={item.productname}
@@ -27,30 +61,9 @@ const products =await data.products
     </div>
 }
 
-async function productfetch () {
-    try {
-        //getting products
-          const res = await fetch('http://localhost:3000/api/gettingproducts',{
-        method:'GET'
-    })
-    if(res.ok){
-       
-         const products= await res.json()
-         console.log(products)
-         return products
-    }
-    return res.headers
-    } catch (error) {
-              console.log(error)
-return{error:error}
 
-    }
-
-  
- 
-   
-}
 //getting product data using a fetch and then giving to props of components to render it,
 //it should be an array of objects to map through
 //its and object called product that contains an array so it data.product.map that maps through the items and 
-//it couls be a component structutre that takes props or it could be an array
+//it could be a component structutre that takes props or it could be an array
+//first i used fetch,then i used it inside useeffect, i wanna use react query,
--- src/components/Productcard.tsx
+++ src/components/Productcard.tsx
@@ -1,12 +1,22 @@
+import Image from "next/image";
+import pic from '@/app/1.jpg'
 
 export default function Productcard({productname,price,productid}){
 
-    return <div  className="w-[15rem]">
-
+    return <div  className="w-[20rem] mt-4 ml-7">
+<div className="">
+<Image
+        src={pic}
+        width={600}
+        height={500}
+        alt="dd"
+        className=""
+        />
+</div>
 {productid}
-        <div className="flex">
+        <div className="  ">
              <p>{price}</p>
-             <p className="">{productname}</p>
+             <p className="text-right">{productname}</p>
              </div>
        
 
