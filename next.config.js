bf9fe672feb7b96c682b3cf364a5e97344fa3c6f
navid <navid.aminzadeh@gmail.com>
2025-04-23 12:20:24 +0330

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,60 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
-images:{
-    remotePatterns:[
-    {
-        protocol:'https',
-        hostname:'ecommercemountain.storage.iran.liara.space'
-    }
-]
-}
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'ecommercemountain.storage.iran.liara.space'
+            }
+        ],
+        minimumCacheTTL: 60,
+        formats: ['image/webp'],
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    },
+    poweredByHeader: false,
+    compress: true,
+    reactStrictMode: true,
+    swcMinify: true,
+    compiler: {
+        removeConsole: process.env.NODE_ENV === 'production',
+    },
+    experimental: {
+        optimizeCss: true,
+    },
+    headers: async () => {
+        return [
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'X-DNS-Prefetch-Control',
+                        value: 'on'
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=63072000; includeSubDomains; preload'
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block'
+                    },
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'SAMEORIGIN'
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff'
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'origin-when-cross-origin'
+                    }
+                ],
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
